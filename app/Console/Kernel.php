92a064bb2340d4f0a993bb4cf2987edfaff56654
Zain Mehdi <szainmehdi@gmail.com>
2016-06-11 19:34:12 -0700

    Add command to sync production DB to dev.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
          Commands\SetupTeamworkAccount::class,
          Commands\MigrateReportrData::class,
          Commands\ModelFactory::class,
+         Commands\SyncDatabase::class,
     ];
 
     /**
